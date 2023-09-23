28f10ff4d57c58e6dc15cb038558434e9edb268d
squirtlemon <elias.marcon8@gmail.com>
2023-09-23 18:35:27 +0200

    Performance mode für quickhull eingebaut

--- prod_files/performance_mode.py
+++ prod_files/performance_mode.py
@@ -1,40 +1,18 @@
-# imports for the algorithms
-from divide_and_conquer_algorithm.divide_and_conquer import *
-
-
 import tkinter as tk
 import random
 import time
+import pandas as pd
 
-
-from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText  # Import ScrolledText for multi-line text display
 from tkinter.font import Font
+from tkinter import ttk, filedialog
 
-
-
-## Create a list to store the convex hull points
-convex_hull_points = []
 
 points = [] # Initialize an empty list to store points
 
 # Variables to store the elapsed times of each algorithm
 divide_and_conquer_time = 0.0
 algorithm2_time = 100.0
-
-
-def add_point():
-    
-    point_str = point_entry.get()
-    
-    try:
-        
-        x, y = map(float, point_str.split(","))
-        points.append((x, y))
-        point_entry.delete(0, tk.END)  # Clear the input field after adding a point
-        display_point_count()
-    except ValueError:
-        pass  # Handle invalid input
 
 def add_random_points():
 
@@ -52,10 +30,6 @@ def add_random_points():
     except ValueError:
         pass
 
-def add_point_on_enter(event):
-
-    add_point()
-
 def add_random_points_on_enter(event):
 
     add_random_points()    
@@ -69,7 +43,7 @@ def clear_points():
 
 # Create a function to display the current number of points
 def display_point_count():
-    point_count_label.config(text=f"Number of Points: {len(points)}")
+    point_count_label.config(text=f"Number of Points: {len(points):,.0f}")
 
 
 # Function to run two commands
@@ -83,22 +57,67 @@ def run_both_commands():
 
 # Function to update the faster_algorithm_label
 def update_faster_algorithm_label():
+    
     global divide_and_conquer_time, algorithm2_time
     
     if divide_and_conquer_time < algorithm2_time:
+
         faster_algorithm_label.config(text=f"Divide and Conquer Algorithm with {divide_and_conquer_time:.4f} seconds")
+    
     else:
+
         faster_algorithm_label.config(text=f"Algorithm 2 with {algorithm2_time:.4f} seconds")
 
 # Function that does nothing when the button is clicked
 def do_nothing():
     pass
 
+def center_window(root):
+    
+    window_width = int(root.winfo_screenwidth() * 0.5) 
+    window_height = int(root.winfo_screenwidth() * 0.5)
+
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    x = ( screen_width / 2 ) - ( window_width / 2 )
+    y = ( screen_height / 2 ) - ( window_height / 2 )
+
+    root.geometry(f'{window_width}x{window_height}+{int(x)}+{int(y)}')
+
+# Function to handle file selection and reading
+def open_file():
+
+    file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
+    
+    if file_path:
+
+        with open(file_path, 'r') as file:
+            
+            lines = file.readlines()
+            
+            if len(lines) >= 2:
+
+                try:
+                    # Read the number of points from the first line
+                    num_points = int(lines[0])
+                    
+                    # Parse the following lines with x,y coordinates
+                    for line in lines[1:]:
+                        x, y = map(float, line.strip().split(','))
+                        points.append((x, y))
+                    
+                except ValueError:
+                    num_points_label.config(text="Invalid file format")
+            else:
+                num_points_label.config(text="Invalid file format")
+
+            display_point_count()
+
 
 ##########################################################################################################################
 # Divide and Conquer Methods
 
-## OPTIONL ? MERGESORT SELBER IMPLEMENTIEREN ODER EINFACH SORT() BENUTZEN?
 def divide_and_conquer_convex_hull():
     
     # Record the start time
@@ -107,22 +126,11 @@ def divide_and_conquer_convex_hull():
     points.sort()
 
     global convex_hull_points
-    convex_hull_points = divide(points)
+    # convex_hull_points = divide(points)
 
     global divide_and_conquer_time 
     # Calculate the elapsed time
     divide_and_conquer_time = time.time() - start_time
-
-    # Clear existing text in the Text widget
-    dac_convex_hull_text.delete(1.0, tk.END)
-    convex_hull_text.delete(1.0, tk.END)
-    
-    # Insert the convex hull points into the Text widget
-    for point in convex_hull_points:
-
-        dac_convex_hull_text.insert(tk.END, f"({point[0]}, {point[1]})\n")
-        convex_hull_text.insert(tk.END, f"({point[0]}, {point[1]})\n")
-    
 
     # Update the elapsed time label
     dac_elapsed_time_label.config(text=f"Elapsed Time Divide And Conquer Algorithm: {divide_and_conquer_time:.4f} seconds")
@@ -137,7 +145,7 @@ def divide_and_conquer_convex_hull():
 
 
 ##########################################################################################################################
-# Second Algorithm
+# Quickhull
 
 
 
@@ -149,66 +157,140 @@ def divide_and_conquer_convex_hull():
 root = tk.Tk()
 root.title("Performance Optimized Mode")
 
+center_window(root)  # Center the main window
+
 # Create GUI elements
-point_label = ttk.Label(root, text="Enter Points (x.0, y.0):")
-point_label.grid(row=0, column=0, padx=5, pady=5, columnspan=2)
-
-point_entry = ttk.Entry(root)
-point_entry.grid(row=1, column=0, padx=5, pady=5, columnspan=2)
-point_entry.bind("<Return>", add_point)  # Bind the Enter key to add_point
-
-ttk.Button(root, text="Add Point", command=add_point).grid(row=2, column=0, padx=5, pady=5, columnspan=2)
-
-# Input field for specifying the number of random points
-num_points_label = ttk.Label(root, text="Enter the number of random points:")
-num_points_label.grid(row=0, column=2, padx=5, pady=5, columnspan=2)
-
-num_points_entry = ttk.Entry(root)
-num_points_entry.grid(row=1, column=2, padx=5, pady=5, columnspan=2)
-num_points_entry.bind("<Return>", add_random_points)  # Bind the Enter key to add_random_points
-
-add_random_button = ttk.Button(root, text="Add Random Points", command=add_random_points)
-add_random_button.grid(row=2, column=2, padx=5, pady=5, columnspan=2)
-
-ttk.Button(root, text="Divide and Conquer Algorithm", command=divide_and_conquer_convex_hull).grid(row=3, column=0, padx=5, pady=5)
-ttk.Button(root, text="Algorithm 2", command=do_nothing).grid(row=3, column=1, padx=5, pady=5)
-ttk.Button(root, text="Compare both Algorithms", command=run_both_commands).grid(row=3, column=2, padx=5, pady=5)
-ttk.Button(root, text="Clear Points", command=clear_points).grid(row=3, column=3, padx=5, pady=5)  # Button to clear points
 
 # Create a label to display the point count
 point_count_label = ttk.Label(root, text="Number of Points: 0")
-point_count_label.grid(row=4, column=0, columnspan=4, padx=10, pady=10)
+point_count_label.grid(row=0, column=0, columnspan=8, padx=10, pady=10)
+
+# Input field for specifying the number of random points
+num_points_label = ttk.Label(root, text="Enter the number of random points:")
+num_points_label.grid(row=1, column=0, padx=5, pady=5, columnspan=4)
+
+num_points_entry = ttk.Entry(root)
+num_points_entry.grid(row=1, column=4, padx=5, pady=5, columnspan=4)
+num_points_entry.bind("<Return>", add_random_points_on_enter)  # Bind the Enter key to add_random_points
+
+
+ttk.Button(root, text="Clear Points", command=clear_points).grid(row=2, column=0, columnspan=3, padx=5, pady=(5, 20))  # Button to clear points
+
+# Create a button to trigger file upload
+upload_button = ttk.Button(root, text="Upload File", command=open_file)
+upload_button.grid(row=2, column=3, columnspan=2, padx=5, pady=5)
+
+add_random_button = ttk.Button(root, text="Add Random Points", command=add_random_points)
+add_random_button.grid(row=2, column=6, padx=5, pady=(5, 20), columnspan=3)
+
+
+
+
+ttk.Button(root, text="Quickhull Algorithm", command=do_nothing).grid(row=3, column=0, padx=5, pady=5)
+ttk.Button(root, text="Divide and Conquer Algorithm", command=divide_and_conquer_convex_hull).grid(row=3, column=3, padx=5, pady=5)
+ttk.Button(root, text="Compare both Algorithms", command=run_both_commands).grid(row=3, column=6, padx=5, pady=5)
+
+
+###################################################################################################################################
+###################################################################################################################################
+###################################################################################################################################
 
 # Create a custom bold font
-bold_font = Font(family="Helvetica", size=12, weight="bold")
+bold_font = Font(family="Helvetica", size=10, weight="bold")
 
-
-# Create Text widgets to display the points from the convex hull
-dac_convex_hull_label = ttk.Label(root, text="Divide And Conquer Convex Hull Points:")
-dac_convex_hull_label.grid(row=5, column=0, columnspan=4, padx=10, pady=5)
-
-dac_convex_hull_text = ScrolledText(root, height=10, width=40)
-dac_convex_hull_text.grid(row=6, column=0, columnspan=4, padx=10, pady=5)
 
 # Create a label to display the elapsed time
-dac_elapsed_time_label = ttk.Label(root, text="Elapsed Time: 0.0000 seconds", font=bold_font)
-dac_elapsed_time_label.grid(row=7, column=0, columnspan=4, padx=10, pady=10)
-
-
-# Create Text widgets to display the points from the convex hull
-convex_hull_label = ttk.Label(root, text="Algorithm 2 Convex Hull Points:")
-convex_hull_label.grid(row=8, column=0, columnspan=4, padx=10, pady=5)
-
-convex_hull_text = ScrolledText(root, height=10, width=40)
-convex_hull_text.grid(row=9, column=0, columnspan=4, padx=10, pady=5)
+dac_elapsed_time_label = ttk.Label(root, text="Elapsed Time Divide and Conquer: 0.0000 seconds", font = bold_font)
+dac_elapsed_time_label.grid(row=5, column=0, columnspan=8, padx=10, pady=10)
 
 # Create a label to display the elapsed time
-elapsed_time_label = ttk.Label(root, text="Elapsed Time: 0.0000 seconds", font=bold_font)
-elapsed_time_label.grid(row=10, column=0, columnspan=4, padx=10, pady=10)
+elapsed_time_label = ttk.Label(root, text="Elapsed Time Quickhull: 0.0000 seconds", font=bold_font)
+elapsed_time_label.grid(row=6, column=0, columnspan=8, padx=10, pady=10)
 
 # Create a label to display the faster algorithm
-faster_algorithm_label = ttk.Label(root, text="Faster Algorithm: N/A", font=bold_font)
-faster_algorithm_label.grid(row=11, column=0, columnspan=4, padx=10, pady=10)
+faster_algorithm_label = ttk.Label(root, text="Faster Algorithm in this Test Case: N/A", font=bold_font)
+faster_algorithm_label.grid(row=7, column=0, columnspan=8, padx=10, pady=(10, 40))
+
+
+###################################################################################################################################
+############################################## Dataframes for Test Cases ##########################################################
+###################################################################################################################################
+
+# Create a label to display the faster algorithm
+dataframes_general_label = ttk.Label(root, text="Dataframes of Test Cases for Quickhull and Divide and Conquer Algorithm", font=Font(family="Helvetica", size=10, weight="bold"))
+dataframes_general_label.grid(row=8, column=0, padx=10, columnspan=8, pady=(10, 20))
+
+###################################################################################################################################
+
+# Create a label to display the faster algorithm
+quickhull_dataframe_label = ttk.Label(root, text="Quickhull Dataframe of Test Cases", font=Font(family="Helvetica", size=10))
+quickhull_dataframe_label.grid(row=9, column=0, columnspan=8, padx=5, pady=5)
+
+# Read the DataFrame from a CSV file
+df = pd.read_csv(r'C:\Users\User\OneDrive - FH Technikum Wien\1_Semester\Advanced_Programming\Convex_Hull\quickhull\quickhull_performance\Testfile_Quickhull.csv', sep = ",")
+df = df.sort_values(by='Number_of_Points', ascending=False)
+
+# Create a Treeview widget to display the DataFrame
+tree = ttk.Treeview(root, columns=list(df.columns), show='headings')
+
+# Add column headings
+for col in df.columns:
+    tree.heading(col, text=col)
+    tree.column(col, width=150)  # Adjust the width as needed
+
+# Insert DataFrame rows into the Treeview
+for i, row in df.iterrows():
+    tree.insert('', 'end', values=row.tolist())
+
+# Create a scrollbar
+scrollbar = ttk.Scrollbar(root, orient='vertical', command=tree.yview)
+tree.configure(yscroll=scrollbar.set)
+
+# Pack the Treeview and scrollbar into the window
+tree.grid(row=10, column=0, columnspan=8, padx=10, pady=(10, 30))
+scrollbar.grid(row=10, column=8, columnspan=8, padx=10, pady=(10, 30))
+
+###################################################################################################################################
+
+# Create a label to display the faster algorithm
+divide_and_conquer_dataframe_label = ttk.Label(root, text="Divide and Conquer Dataframe of Test Cases", font=Font(family="Helvetica", size=10))
+divide_and_conquer_dataframe_label.grid(row=11, column=0, columnspan=8, padx=5, pady=5)
+
+# Read the DataFrame from a CSV file
+df_divide_and_conquer = pd.read_csv(r'C:\Users\User\OneDrive - FH Technikum Wien\1_Semester\Advanced_Programming\Convex_Hull\quickhull\quickhull_performance\Testfile_Quickhull.csv', sep = ",")
+df_divide_and_conquer = df_divide_and_conquer.sort_values(by='Number_of_Points', ascending=False)
+
+# Create a Treeview widget to display the DataFrame
+tree = ttk.Treeview(root, columns=list(df_divide_and_conquer.columns), show='headings')
+
+# Add column headings
+for col in df_divide_and_conquer.columns:
+    tree.heading(col, text=col)
+    tree.column(col, width=150)  # Adjust the width as needed
+
+# Insert DataFrame rows into the Treeview
+for i, row in df_divide_and_conquer.iterrows():
+    tree.insert('', 'end', values=row.tolist())
+
+# Create a scrollbar
+scrollbar = ttk.Scrollbar(root, orient='vertical', command=tree.yview)
+tree.configure(yscroll=scrollbar.set)
+
+# Pack the Treeview and scrollbar into the window
+tree.grid(row=10, column=0, columnspan=8, padx=10, pady=(10, 30))
+scrollbar.grid(row=10, column=8, columnspan=8, padx=10, pady=(10,30))
+###################################################################################################################################
+
+# Create a label to display the faster algorithm
+generell_faster_label = ttk.Label(root, text="Overall faster Algorithm", font=Font(family="Helvetica", size=10))
+generell_faster_label.grid(row=15, column=0, columnspan=8, padx=5, pady=5)
+
+# Berechnungen
+
+
+###################################################################################################################################
+###################################################################################################################################
+###################################################################################################################################
 
 
 # Run the Tkinter main loop for the performance optimized mode
